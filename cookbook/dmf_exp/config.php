63767514e236fa21864589c0c67c8d251fe2c264
kpx <webmaster@danmaku.us>
2011-10-14 00:50:08 +0800

    移动常见视频源设定到class.VideoSourceSet.php

--- cookbook/dmf_exp/config.php
+++ cookbook/dmf_exp/config.php
@@ -12,15 +12,4 @@ $CheckPerfs = FALSE;
 $EnableAutoTimeShift = TRUE;
 $TimeShiftDelta = 0.000001;
 
-$VideoSourceSet
-	->add('nor'		, new XinaSource())
-	->add('td'		, new TuDouSource())
-	->add('qq'		, new QQSource())
-	->add('6cn'		, new sixRoomSource())
-	->add('local'	, new LocalSource())
-	->add('link'	, new URLSource())
-	->add('url'		, new URLSource())
-	->add('burl'	, new BURLSource())
-	->add('blink'	, new BURLSource());
-
 $HTMLHeaderFmt['playerScripts'] = "\n".'<script type="text/javascript" src="/static/min/b=static&amp;f=jquery-1.6.1.min.js,qule.js,swfobject.js,jquery-ui-1.8.14.custom.min.js,pdm-bili.js"></script>'."\n";
--- cookbook/dmf_exp/inc/class.VideoSourceSet.php
+++ cookbook/dmf_exp/inc/class.VideoSourceSet.php
@@ -8,3 +8,13 @@ class VideoSourceSet extends Set
 }
 
 $VideoSourceSet = new VideoSourceSet();
+$VideoSourceSet
+	->add('nor'		, new XinaSource())
+	->add('td'		, new TuDouSource())
+	->add('qq'		, new QQSource())
+	->add('6cn'		, new sixRoomSource())
+	->add('local'	, new LocalSource())
+	->add('link'	, new URLSource())
+	->add('url'		, new URLSource())
+	->add('burl'	, new BURLSource())
+	->add('blink'	, new BURLSource());
