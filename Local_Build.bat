c1168c22cefd623624b4a8f18b36c0c04042c2e3
kpx <webmaster@danmaku.us>
2011-10-13 23:24:48 +0800

    更新生成器

--- Local_Build.bat
+++ Local_Build.bat
@@ -1,35 +1,46 @@
 @echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
-ECHO DEBUG MODE ON
-ECHO+
-::用于构建本地播放器环境
-SET ROOT_D=.\
-SET ROOT_F="googlec5308d0d9cb559ed.html","H.php","robots.txt","roadmap.txt","robots.txt"
-SET CONFIG_D=.\local
-SET CONFIG_F="Main.FNQ.php","Queue.php","Main.N1c0.php","Main.Flvcache.php"
-SET PLAYER_D=.\p
-SET PLAYER_F=".\ac",".\ni"
-SET PMPAGE_D=.\wiki.d
-SET PMPAGE_F=".\Queue",".\Parts",".\Acfun2",".\Bilibili2",".\DMR"
-SET PROC_ARR=CONFIG,PLAYER,PMPAGE,ROOT
-FOR %%a IN (%PROC_ARR%) DO (
-    pushd !%%a_D!
-    FOR %%b IN (!%%a_F!) DO (
-        ECHO 	!CD! : DEL /F /Q %%b
-    )
-    popd
+pause
+echo 清除项目文件
+call :RemoveDir .\settings
+call :DeleteFiles .\.buildpath .\.gitignore .\.project 
+echo 清理辅助文件
+call :DeleteFiles .\APIMap.txt .\FindAll.cmd .\wiki.d\.pageindex
+echo 清理非公开文件
+call :DeleteFiles .\googlec5308d0d9cb559ed.html .\FindAll.cmd
+pushd local
+call :DeleteFiles .\Main.Talk.php .\Queue.php .\Main.FNQ.php .\Main.Flvcache.php
+popd
+echo 清理无用数据文件
+call :clearDir .\uploads\Acfun2
+call :clearDir .\uploads\Bilibili2
+call :clearDir .\wiki.d\Acfun2
+call :clearDir .\wiki.d\Bilibili2
+call :clearDir .\wiki.d\DMR
+call :RemoveDir .\wiki.d\Queue
+call :RemoveDir .\wiki.d\Site
+call :RemoveDir .\wiki.d\SiteAdmin
+echo 清理备份文件
+for /R .\ %%a in (*.bak) do (
+    call :DeleteFiles  %%a
 )
-ECHO CLEAN UP  FIN.
-SET LIBMOVE_D=Main,Site,System
-FOR %%a IN (%LIBMOVE_D%) DO (
-	ECHO 	!CD! : COPY ".\wiki.d\%%a\*.*" ".\shared\dmflib.d\%%a\"
-	
-	SET DELETE_D=".\wiki.d\%%a\",!DELETE_D!
-)
-SET DELETE_D=!DELETE_D!".\p\ac",".\p\ni"
-ECHO MOVE PAGE FIN.
-FOR %%a IN (%DELETE_D%) DO (
-	ECHO 	!CD! : RD /S /Q %%a
-)
-ECHO DELETE DIR FIN.
+
+
 PAUSE
+GOTO :EOF
+
+:clearDir
+echo    清空 "%~f1"
+del "%~f1\*.*"
+GOTO :EOF
+
+:RemoveDir
+echo    移除 "%~f1"
+rd "%~f1" /q
+GOTO :EOF
+
+:DeleteFiles
+if "%~x1"=="" GOTO :EOF
+echo    删除 "%~x1"
+del "%~f1" /F /Q
+shift /1
+GOTO :DeleteFiles
