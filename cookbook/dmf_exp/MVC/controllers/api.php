0444b8e1d4d66b6f45c405b8a01e16d9e7e2b5d5
kpx <webmaster@danmaku.us>
2011-11-19 02:00:45 +0800

    修正Acfun的GroupFooter问题

--- cookbook/dmf_exp/MVC/controllers/api.php
+++ cookbook/dmf_exp/MVC/controllers/api.php
@@ -1,2 +1,24 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 //Acfun (老) 播放器接口
+class Api extends CI_Controller {
+    public function filtrate()
+    {
+        die('<?xml version="1.0" encoding="utf-8"?><information>
+<filtrate name="普通关键字"><data>费大人</data></filtrate><filtrate name="超级关键字"><data>费大人</data></filtrate></information>');
+    }
+    
+    public function filtrate2()
+    {
+        die('<?xml version="1.0" encoding="utf-8"?><information><data>taobao</data></information>');
+    }    
+
+    public function ujson()
+    {
+        die('[]');
+    }
+    
+    public function badwords()
+    {
+        die('[]');
+    }
+}
