3d20184497eb9045ed0f614d796133f6c0114128
kpx <webmaster@danmaku.us>
2011-11-19 14:10:15 +0800

    X

--- local/config.php
+++ local/config.php
@@ -92,51 +92,12 @@ $XESTagAuth = 'edit';
 include_once("$FarmD/cookbook/tagpages.php");
 $WikiStyleCSS[] = 'line-height';
 
-# 插件设定
-## 外链审核
 //include_once("$FarmD/scripts/urlapprove.php");
 $UrlLinkFmt = "<a class='urllink' href='\$LinkUrl' >\$LinkText</a>";
 $GroupHeaderFmt =
   '(:include {$SiteGroup}.AllGroupHeader:)(:nl:)'
   .'(:include {$Group}.GroupHeader:)(:nl:)';
 
-## 编辑通告
-/*
-if (($_GET['action'] != 'setdef') && ($_GET['action'] != 'GetFlvUrl')) {$EnableNotify = 1;}
-$NotifySquelch = 60;
-$NotifyDelay = 300;
-$NotifyItemFmt = " * \$FullName . . . \$PostTime by \$Author : \n \$PageUrl"; 
-$EnableNotifySubjectEncode = 1;
-$NotifyHeaders = "Content-type: text/plain; charset=$Charset"; 
-*/
-
-## RSS & 网站地图
-/*
-if ( $action=="sitemap" ) {
-	$RssMaxItems=50000;
-	$RssSourceSize=0;
-	$RssDescSize=0;
-	$action="rss";
-	SDVA($FeedTrailOpt, array('trail' => $pagename, 'count' => 50000));
-}
-include_once("$FarmD/scripts/feeds.php");
-*/
-
-## 游客记录
-/*
-$VisitorsLoggingDirectory = "./uploads/VisitorsLog";
-$VisitorsLoggingFileName = "%Y-%m-%d.txt";
-$VisitorsLoggingPurgeAfterDays = 0;
-$VisitorsLoggingFormat = '%Date %Time %RemoteAddr:pad %Action:pad '
-    . '%HttpHost %WikiGroup.%WikiPage '
-    . '"%HttpReferer" "%HttpUserAgent"' . "\n";
-$VisitorsLoggingDateFormat = '%Y-%m-%d';
-$VisitorsLoggingTimeFormat = '%H:%M:%S';
-$VisitorsLoggingIgnoreList = array('127.0.0.1');
-include_once("$FarmD/cookbook/visitorslogging1337.php");
-*/
-
-# END
 
 include("./cookbook/dmf_exp/DMF.php");
 
